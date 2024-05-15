4b3da1ed9b7482fc6207917c9acc784a2b7e03ce
Muh Reyzki <muhreyzki11@gmail.com>
2024-05-15 12:07:15 +0700

    feat: fetch data in product list

--- components/Productlist.tsx
+++ components/Productlist.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Card from "./ui/Card";
+import { Product } from "@/types/product";
 
-const Productlist = () => {
+const Productlist: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/products"); // Ganti dengan URL API Anda
+        const data: Product[] = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="bg-white h-full w-full p-4 grid grid-cols-2 gap-4">
-      <Card></Card>
-      <Card></Card>
+      {products.map((product) => (
+        <Card key={product.id} product={product} />
+      ))}
     </div>
   );
 };
--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,24 +1,34 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { Product } from "@/types/product";
 
-const Card = () => {
+interface cardItem {}
+
+interface CardProps {
+  product: Product;
+}
+
+const Card: React.FC<CardProps> = ({ product }) => {
   return (
     <div className="flex flex-col p-[10px] bg-[#F9F8F6] gap-4">
       <div className="flex flex-col gap-4">
         <Image
-          src="/products/cucumber.png"
-          alt="image1"
+          src={product.imageUrl}
+          alt={product.name}
           width={145}
           height={113}
           className="mix-blend-multiply bg-transparent"
         />
         <div>
-          <h2 className="font-semibold text-xl">$3.2</h2>
-          <p className="font-light">Cucumbers</p>
+          <h2 className="font-semibold text-xl">
+            ${(product.price * 100).toFixed(2)}
+          </h2>
+          <p className="font-light">{product.name}</p>
         </div>
       </div>
       <div className="flex justify-between items-center">
-        <p>1kg</p>
+        <p>{(product.weight / 1000).toFixed(1)}kg</p>
         <button className="rounded-full p-2">
           <Image src="/24/add product.svg" alt="addic" width={24} height={24} />
         </button>
